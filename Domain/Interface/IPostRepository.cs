0fd5787e0f9e217b4bd99d2279c4a2905cc05f9f
PatiDev95 <54117153+PatiDev95@users.noreply.github.com>
2021-02-15 17:22:37 +0100

    Dodaj pliki projektów.

---
+++ Domain/Interface/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entity;
+using System.Collections.Generic;
+
+namespace Domain.Interface
+{
+    public interface IPostRepository
+    {
+        IEnumerable<Post> GetAll();
+        Post GetById(int id);
+        Post Add(Post post);
+        void Update(Post post);
+        void Delete(Post post);
+    }
+}
---
+++ Infrastructure/Repository/PostRepository.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entity;
+using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infrastructure.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        public static readonly ISet<Post> _posts = new HashSet<Post> 
+        { 
+            new Post(1, "Title1", "Content1"),
+            new Post(2, "Title2", "Content2"),
+            new Post(3, "Title3", "Content3")
+        };
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _posts; 
+        }
+
+        public Post GetById(int id)
+        {
+            return _posts.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Post Add(Post post)
+        {
+            post.Id = 1 + _posts.Count;
+            post.CreatedAt = DateTime.UtcNow;
+           _posts.Add(post);
+            return post;
+        }
+
+        public void Update(Post post)
+        {
+            post.LastModified = DateTime.UtcNow;
+        }
+
+        public void Delete(Post post)
+        {
+            _posts.Remove(post);
+        } 
+    }
+}
